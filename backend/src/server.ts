fa8012e9cd807929654c271a9522595f7098a583
Asam237 <abbasalimokolo@gmail.com>
2023-06-07 07:43:35 +0100

    add server routes for organisation

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express, { Application } from "express"
 import * as bodyParser from "body-parser"
 import cors from "cors"
 import { UserRoute } from "./routes/user.route"
+import { OrganisationRoute } from "./routes/organisation.route"
 
 export const setupRestEndpoints = (app: Application) => {
     const router = express.Router()
@@ -10,4 +11,5 @@ export const setupRestEndpoints = (app: Application) => {
     app.use(cors())
     app.use("/", router)
     app.use("/", UserRoute())
+    app.use("/", OrganisationRoute())
 }
