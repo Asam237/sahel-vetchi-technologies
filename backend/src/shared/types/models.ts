67d69febeb9055a2bcdc4b60895a52a09d6a53f3
Asam237 <abbasalimokolo@gmail.com>
2023-06-07 06:54:32 +0100

    add organisation type

--- backend/src/shared/types/models.ts
+++ backend/src/shared/types/models.ts
@@ -12,3 +12,13 @@ export type LoginUserInput = {
   email: string;
   password: string;
 };
+
+export type CreateOrganisationInput = {
+  fullname: string
+  phone: string
+  email: string
+  adress: string
+  code: string
+  isRoot: boolean
+  user: any
+}
