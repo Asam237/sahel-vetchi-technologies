703eedef9101ceea1c3b68578946d76eca0a6038
Asam237 <abbasalimokolo@gmail.com>
2023-06-07 06:02:47 +0100

    add user model

---
+++ backend/src/domain/models/user.model.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema: mongoose.Schema = new mongoose.Schema({
+  fullname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+  },
+  adress: {
+    type: String,
+  },
+  userType: {
+    type: String,
+    enum: ["ADMIN_ROOT", "ADMIN_ORG", "METIER_ORG"],
+    default: "METIER_ORG",
+  },
+  isLocked: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const UserModel = mongoose.model("User", userSchema);
+
+export { UserModel };
