7c121e2c2917b565ba99414b7e703c437a5df544
Asam237 <abbasalimokolo@gmail.com>
2023-06-07 06:24:42 +0100

    add server routes

---
+++ backend/src/server.ts
@@ -0,0 +1,13 @@
+import express, { Application } from "express"
+import * as bodyParser from "body-parser"
+import cors from "cors"
+import { UserRoute } from "./routes/user.route"
+
+export const setupRestEndpoints = (app: Application) => {
+    const router = express.Router()
+    app.use(bodyParser.json())
+    app.use(express.json())
+    app.use(cors())
+    app.use("/", router)
+    app.use("/", UserRoute)
+}
