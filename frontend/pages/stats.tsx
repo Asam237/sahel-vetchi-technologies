ff59b5eb40085cf3527cacca120781d048f1e944
Asam237 <abbasalimokolo@gmail.com>
2023-06-11 08:30:40 +0100

    add stats

--- frontend/pages/stats.tsx
+++ frontend/pages/stats.tsx
@@ -1,13 +1,39 @@
 import { Poppins } from "@next/font/google";
+import { useQuery } from "@tanstack/react-query";
 import Head from "next/head";
+import { useState } from "react";
+import { useCookies } from "react-cookie";
 import { BiTrash } from "react-icons/bi";
 import { Footer } from "../components/commons/footer.common";
 import { Header } from "../components/commons/header.common";
 import { Items } from "../components/commons/items.commont";
+import { getAllProduct } from "./api";
 
 const poppins = Poppins({ weight: "400", subsets: ['latin'] })
 
 export default function Stats() {
+
+    const [cookie, removeCookie]: any = useCookies(["qwer"])
+    const token = cookie?.qwer?.token
+
+    const { isLoading, error, data } = useQuery({
+        queryKey: ["products"],
+        queryFn: () => getAllProduct(token)
+    })
+
+
+    const products = data?.data || []
+    let countSold = 0;
+    let countAvailable = 0;
+    products.forEach((item: any) => {
+        if (item?.sale === true) {
+            countSold += 1
+        }
+        if (item?.sale === false) {
+            countAvailable += 1
+        }
+    });
+
     return (
         <>
             <Head>
@@ -19,10 +45,15 @@ export default function Stats() {
             <main className={`px-4 md:px-0 ${poppins.className}`}>
                 <Header />
                 <div className='py-16 container'>
-                    <div className='grid-cols-1 sm:grid-cols-2 md:grid-cols-3 grid gap-x-4 gap-y-10'>
-                        <Items _id={""} descriptionItem={"description"} nameItem={"Prado 2015"} prixAchatItem={"25 000 000 FCFA"} createdAt={"create"} />
-                        <Items _id={""} descriptionItem={"description"} nameItem={"Prado 2015"} prixAchatItem={"25 000 000 FCFA"} createdAt={"create"} />
-                        <Items _id={""} descriptionItem={"description"} nameItem={"Prado 2015"} prixAchatItem={"25 000 000 FCFA"} createdAt={"create"} />
+                    <div className='grid-cols-1 sm:grid-cols-2 grid gap-x-4 gap-y-10'>
+                        <div className="px-8 py-8 border rounded-md shadow-lg hover:shadow-2xl">
+                            <h1 className="text-3xl text-blue-700 font-semibold leading-tight text-center">Products Sold</h1>
+                            <h1 className="text-4xl md:text-6xl text-blue-700 font-bold leading-tight text-center mt-4">{countSold}</h1>
+                        </div>
+                        <div className="px-8 py-4 border rounded-md shadow-lg hover:shadow-2xl">
+                            <h1 className="text-3xl text-blue-700 font-semibold leading-tight text-center">Available Products</h1>
+                            <h1 className="text-4xl md:text-6xl text-blue-700 font-bold leading-tight text-center mt-4">{countAvailable}</h1>
+                        </div>
                     </div>
                 </div>
             </main>
